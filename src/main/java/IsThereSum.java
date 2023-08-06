ee4d71eb4cc45881ceae3b3d56198497c18988a3
Alexander Yu <alexyu299@hotmail.com>
2023-08-06 06:11:27 +0000

    Commit and refresh for tests

--- src/main/java/IsThereSum.java
+++ src/main/java/IsThereSum.java
@@ -10,6 +10,13 @@ public class IsThereSum {
      * @return true if two separate values in the array add up to a target, false otherwise.
      */
     public boolean check(int[] arr, int target){
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+        if (sum == target) {
+            return true;
+        }
         return false;
     }
 }
